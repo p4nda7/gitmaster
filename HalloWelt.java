5bff2dca0edaada77fc89e8a9c086528c513e638
p4nda7 <x0568162@gmail.com>
2025-05-25 03:01:39 +0200

    c3

--- HalloWelt.java
+++ HalloWelt.java
@@ -2,5 +2,6 @@ public class HalloWelt {
     public static void main(String[] args) {
         System.out.println("Ich bin Commit 1");
         System.out.println("Ich bin Commit 2");
+        System.out.println("Ich bin Commit 3");
     }
 }
