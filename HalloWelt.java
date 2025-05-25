876005cff56031539d44cbc958cb15ffaf62f17d
p4nda7 <x0568162@gmail.com>
2025-05-25 02:12:37 +0200

    timesnap

---
+++ HalloWelt.java
@@ -0,0 +1,9 @@
+public class HalloWelt {
+    public static void main(String[] args) {
+        System.out.println("Ich gehöre zum Branch: sandbox-thang");
+        System.out.println("Ich gehöre zum Branch: sandbox-thang");
+        System.out.println("Ich gehöre zum Branch: sandbox-thang");
+        System.out.println("Ich gehöre zum Branch: sandbox-thang");
+        System.out.println("Ich gehöre zum Branch: sandbox-thang");
+    }
+}
--- main.java
+++
@@ -1,5 +0,0 @@
-public class HalloWelt {
-    public static void main(String[] args) {
-        System.out.println("Ich gehöre zum Branch: sandbox-thang");
-    }
-}
