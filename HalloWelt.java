79de7fd391f525a2071b28b3ea46bdfb9ca6816f
p4nda7 <x0568162@gmail.com>
2025-05-25 02:31:04 +0200

    5 lines of commmits

---
+++ HalloWelt.java
@@ -0,0 +1,9 @@
+public class HalloWelt {
+    public static void main(String[] args) {
+        System.out.println("Ich bin Commit 1");
+        System.out.println("Ich bin Commit 1");
+        System.out.println("Ich bin Commit 1");
+        System.out.println("Ich bin Commit 1");
+        System.out.println("Ich bin Commit 1");
+    }
+}
