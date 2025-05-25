19785b61e30cf260c613b46ea6b419bb47bf74f8
p4nda7 <x0568162@gmail.com>
2025-05-25 02:20:59 +0200

    restore cherrypick

--- HalloWelt.java
+++ HalloWelt.java
@@ -1,9 +1,13 @@
 public class HalloWelt {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Ich bin Commit 1");
         System.out.println("Ich bin Commit 1");
         System.out.println("Ich bin Commit 1");
         System.out.println("Ich bin Commit 1");
         System.out.println("Ich bin Commit 1");
+=======
+        System.out.println("Ich will recommitten");
+>>>>>>> 8da9482 (Änderung zu oneline)
     }
 }
