81ae9243ede861c35cdfa5446ebfc114d9606c07
p4nda7 <x0568162@gmail.com>
2025-05-25 03:01:10 +0200

    restore a3176925290914b6575751125e5b660197e31990

--- HalloWelt.java
+++ HalloWelt.java
@@ -2,6 +2,5 @@ public class HalloWelt {
     public static void main(String[] args) {
         System.out.println("Ich bin Commit 1");
         System.out.println("Ich bin Commit 2");
-        System.out.println("Ich bin Commit 3");
     }
 }
